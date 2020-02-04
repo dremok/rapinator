8a49a68764dc0cd1d11b47cab32488c5fdee9eef
dremok <max.y.leander@gmail.com>
2020-02-05 00:23:04 +0100

    decode response

--- api_caller.py
+++ api_caller.py
@@ -1,5 +1,8 @@
+import json
+
 import requests
 
 artist = 'Yung Lean'
 response = requests.get(f'http://localhost:8000/song?{artist}')
-print(response)
+song_dict = json.loads(response.content.decode('UTF-8'))
+print(song_dict['lyrics'])
